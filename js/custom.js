947370e1ff354348e817bc5598da42ccbe40dc4c
Patrick McConnell <paddymcconn1280@hotmail.com>
2016-08-27 12:19:35 +0100

    personal website first go

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+$('.bar-percentage[data-percentage]').each(function (){
+  var progress = $(this);
+  var percentage = Math.ceil($(this).attr('data-percentage'));
+    
+    $({countNum: 0}).animate({countNum: percentage},{
+    duration: 2000,
+    easing:'linear',
+    step: function() {
+      // What todo on every count
+      var pct = Math.floor(this.countNum) + '%';
+      progress.text(pct) && progress.siblings().children().css('width',pct);
+    }
+  });
+});
